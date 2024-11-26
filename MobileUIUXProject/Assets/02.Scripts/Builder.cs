0b02d6b45a0bb452271125f058f5b5fefb4e6cea
marshmar <goekazld123@naver.com>
2024-11-26 17:49:17 +0900

    진행상황 저장

--- MobileUIUXProject/Assets/02.Scripts/Builder.cs
+++ MobileUIUXProject/Assets/02.Scripts/Builder.cs
@@ -7,7 +7,7 @@ public class Builder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 1920x1080 비율로 세로 고정
+        // 1080x1980 비율로 세로 고정
         Screen.SetResolution(540, 960, false); // false는 창 모드
     }
 
